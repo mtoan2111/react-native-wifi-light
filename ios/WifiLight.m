ff21c90e501e96e773b8edc950d0b82d3de5ab61
ToanNgm <toan93.hust@gmail.com>
2020-11-27 10:42:25 +0700

    update signature

--- ios/WifiLight.m
+++ ios/WifiLight.m
@@ -1,19 +1,25 @@
 #import "WifiLight.h"
 
-@implementation WifiLight
+@implementation Wifi
 
 RCT_EXPORT_MODULE()
 
+- (NSArray<NSString *> *)supportedEvents{
+    return @[@"wifiScanResult"];
+}
+
+RCT_EXPORT_METHOD(getWifiList){
+    
+}
+
+RCT_EXPORT_METHOD(startScan){
+    
+}
+
+RCT_EXPORT_METHOD(stopScan){
+    
+}
 // Example method
 // See // https://facebook.github.io/react-native/docs/native-modules-ios
-RCT_REMAP_METHOD(multiply,
-                 multiplyWithA:(nonnull NSNumber*)a withB:(nonnull NSNumber*)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
-{
-  NSNumber *result = @([a floatValue] * [b floatValue]);
-
-  resolve(result);
-}
 
 @end
